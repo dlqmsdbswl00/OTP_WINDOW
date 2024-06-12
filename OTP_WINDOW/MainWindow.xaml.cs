003e17401141745d74f4a099e275328608b23b89
김윤지 <21615008@konyang.ac.kr>
2024-06-12 21:34:57 +0900

    OTP UI 통합 시작

--- OTP_WINDOW/MainWindow.xaml.cs
+++ OTP_WINDOW/MainWindow.xaml.cs
@@ -23,6 +23,42 @@ namespace OTP_WINDOW
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new IntroPage());
+        }
+
+        private void OTP_LOGO_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new IntroPage());
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // START 버튼 클릭 시 동작 정의
+            MainFrame.Navigate(new StartPage());
+        }
+
+        private void Chapter1Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 1 버튼 클릭 시 동작 정의
+            MainFrame.Navigate(new Chap1_0());
+        }
+
+        private void Chapter2Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 2 버튼 클릭 시 동작 정의
+            //MainFrame.Navigate(new Chap2());
+        }
+
+        private void Chapter3Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 3 버튼 클릭 시 동작 정의
+            //MainFrame.Navigate(new Chap3());
+        }
+
+        private void Chapter4Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 4 버튼 클릭 시 동작 정의
+            //MainFrame.Navigate(new Chap4());
         }
     }
 }
